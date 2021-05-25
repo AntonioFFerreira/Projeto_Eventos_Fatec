593ddf72cce06464ee86f837d8bab81fc08cde9f
AntonioFFerreira <antonio.ferreira7@fatec.sp.gov.br>
2021-05-25 16:57:09 -0300

    Inicio do projeto de eventos

---
+++ src/main/java/com/anenst/springboot/ProjetoFatecEventosApplication.java
@@ -0,0 +1,13 @@
+package com.anenst.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoFatecEventosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoFatecEventosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/anenst/springboot/ProjetoFatecEventosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anenst.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoFatecEventosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
